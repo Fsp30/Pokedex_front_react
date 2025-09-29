7dc4052cc2e40d9405076a7d1d082358946701b0
Fsp30 <depaulacostafilipe@gmail.com>
2025-09-29 20:04:46 -0300

    create opinionListResponseSchema

--- src/schemas/response/opinionSchemaResponse.ts
+++ src/schemas/response/opinionSchemaResponse.ts
@@ -1,7 +1,12 @@
 import { z } from 'zod'
 
 export const opinionResponseSchema = z.object({
-        id: z.string(),
         user_id: z.string(),
         text: z.string(),
+        created_at: z.number() 
+})
+
+export const opinionListResponseSchema = z.object({
+  likes: z.number(),
+  opinions: z.array(opinionResponseSchema)
 })
