fad1ba5ca1ab69a2144e633ea5142ef964ab8691
Fsp30 <depaulacostafilipe@gmail.com>
2025-09-29 20:02:05 -0300

    create opinion Service (function(s): CreateOpinionPokemon and getPokemonOpinions

---
+++ src/services/opinionPokemonService.ts
@@ -0,0 +1,28 @@
+import { api } from '../api_client/axios'
+import { createOpinionInputSchema, listOpinionsInputSchema } from '../schemas/request/pokemonSchemaRequest'
+import { opinionListResponseSchema } from '../schemas/response/opinionSchemaResponse'
+import type { z } from 'zod'
+
+type CreateOpinionInput = z.infer<typeof createOpinionInputSchema>
+type OpinionListResponse = z.infer<typeof opinionListResponseSchema>
+
+export async function CreateOpinionPokemon(pokemon_name: string, params: CreateOpinionInput): Promise<void> {
+        try {
+                const validatedParams = createOpinionInputSchema.parse(params)
+                await api.post(`/pokemon/${pokemon_name}/opinion`, validatedParams)
+                return
+        } catch (err: any) {
+                throw new Error(err.response?.data?.error || err.message || "Error adding opinion to Pokemon")
+        }
+}
+
+export async function getPokemonOpinions(pokemon_name: string): Promise<OpinionListResponse> {
+        try {
+                const validatedInput = listOpinionsInputSchema.parse({ name: pokemon_name })
+                const { data } = await api.get(`/pokemon/${validatedInput.name}/opinions`)
+
+                return opinionListResponseSchema.parse(data)
+        } catch (err: any) {
+                throw new Error(err.response?.data?.error || err.message || "Error fetching Pokemon opinions")
+        }
+}
