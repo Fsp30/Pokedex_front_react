d24eb7d02c2fe6021b91bdeba57d92edae67a463
Fsp30 <depaulacostafilipe@gmail.com>
2025-10-01 20:10:01 -0300

    trade reading/loading environmenst for vite default

--- src/api_client/axios.ts
+++ src/api_client/axios.ts
@@ -1,13 +1,11 @@
-import axios from "axios";
-import dotenv from 'dotenv';
 
-dotenv.config()
-const API_URL = process.env.URL_BACK_END
+import axios from "axios"
 
+const API_URL = import.meta.env.VITE_API_URL_BACK_END || "http://localhost:3000"
 
-export const api = (
-        axios.create({
-                baseURL: API_URL,
-                headers:{ "Content-Type": "application/json"}
-        })
-)
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: { 
+    "Content-Type": "application/json"
+  }
+})
