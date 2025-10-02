dd21ca8f8d3be0617de7ca97a5cc1e326754ebf4
Fsp30 <depaulacostafilipe@gmail.com>
2025-10-02 19:47:07 -0300

    improving schema createOpinionInput, add field pokemon_name

--- src/schemas/request/pokemonSchemaRequest.ts
+++ src/schemas/request/pokemonSchemaRequest.ts
@@ -11,7 +11,9 @@ export const likePokemon = z.object({
 
 export const createOpinionInputSchema = z.object({
         user_id: z.string().min(1),
+        pokemon_name: z.string().min(2, "Very short pokemon name"),
         text: z.string().min(3, "Very shot opinion").max(500, "Opinion too long")
+
 })
 
 export const listOpinionsInputSchema = z.object({
