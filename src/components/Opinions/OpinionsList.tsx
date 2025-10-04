d7f6061a157fb67d8411560eca78b3eb4ba11036
Fsp30 <depaulacostafilipe@gmail.com>
2025-10-04 20:50:23 -0300

    feature: add view pokemon and pokemon name

--- src/components/Opinions/OpinionsList.tsx
+++ src/components/Opinions/OpinionsList.tsx
@@ -1,25 +1,38 @@
 import { useListOpinions } from "../../hooks/useOpinions";
+import { usePokemon } from "../../hooks/usePokemon";
 
 interface PokemonOpinionsProps {
-        name: string
+        name: string;
 }
 
 export function PokemonOpinions({ name }: PokemonOpinionsProps) {
-        const { data: pokemon, isLoading } = useListOpinions(name)
+        const { data: pokemonStats, isPending: isPokemonPending } = usePokemon(name);
+        const { data: pokemonOpinions, isLoading: isOpinionsLoading  } = useListOpinions(name);
 
-        if (isLoading) return <p>Loading...</p>
-        if (!pokemon) return <p>Pokemon não encontrado</p>
-        if (!pokemon?.opinions) return <p>Nenhuma opinião encontrada para {name}</p>
+        if (isOpinionsLoading || isPokemonPending) return <p>Loading...</p>;
+
+        if (!pokemonStats) return <p>Pokemon não encontrado</p>;
+        if (!pokemonOpinions) return <p>Nenhuma opinião encontrada para {name}</p>;
 
         return (
                 <div>
-                        <p><strong>Total de Opiniões:</strong> {pokemon.opinions?.length || 0}</p>
+                        <div>
+                                <h2>{pokemonStats.api_data.name}</h2>
+                                {pokemonStats.api_data.sprites.front_default && (
+                                        <img
+                                                src={pokemonStats.api_data.sprites.front_default}
+                                                alt={`${pokemonStats.api_data.name} front`}
+                                        />
+                                )}
+                        </div>
 
-                        {pokemon.opinions && pokemon.opinions.length > 0 && (
+                        <p><strong>Total de Opiniões:</strong> {pokemonOpinions.opinions?.length || 0}</p>
+
+                        {pokemonOpinions.opinions && pokemonOpinions.opinions.length > 0 && (
                                 <div>
                                         <h3>Opiniões</h3>
                                         <div>
-                                                {pokemon.opinions.map((opinion) => (
+                                                {pokemonOpinions.opinions.map((opinion) => (
                                                         <div key={`${opinion.user_id}-${opinion.created_at}`}>
                                                                 <p><strong>{opinion.user_id}:</strong> {opinion.text}</p>
                                                                 <small>
