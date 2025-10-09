e1f5a43cf7ecd7a60fe0193076f49091a5624fc0
Fsp30 <depaulacostafilipe@gmail.com>
2025-10-09 20:48:01 -0300

    create component Footer

---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,18 @@
+export function Footer() {
+        return (
+                <>
+                        <footer>
+                                <div>
+                                        <p>&copy; 2025 Poke Opinion. Todos os direitos reservados.</p>
+                                        <p>Criado com ❤️ amor por um fã de pokemon, mas muito mais de Golang</p>
+                                        <nav>
+                                                <a href="#">Sobre</a>
+                                                <a href="#">Contato</a>
+                                                <a href="#">Política de Privacidade</a>
+                                                <a href="#">Termos de Uso</a>
+                                        </nav>
+                                </div>
+                        </footer>
+                </>
+        )
+}
