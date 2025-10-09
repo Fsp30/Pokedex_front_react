65c3e60eccb43760590893cab88030d7235e9017
Fsp30 <depaulacostafilipe@gmail.com>
2025-10-09 20:48:47 -0300

    craete Introduction PokeOpinion

---
+++ src/components/global/Intro.tsx
@@ -0,0 +1,31 @@
+export function Intro() {
+        return (
+                <>      
+                        <main>
+                                <section>
+                                <h1> Poke Opinion - A Sua Companheira Pokémon!</h1>
+                                <p>
+                                        Esta é a minha Pokedex pessoal, uma ferramenta incrível que reúne todas as informações
+                                        sobre os Pokémon que você ama! Na minha opinião, esta é uma das melhores Pokedex
+                                        já criadas, com interface intuitiva e recursos fantásticos.
+                                </p>
+
+                                <h2>O que eu mais gosto nesta Pokedex:</h2>
+                                <ul>
+                                        <li>✅ Busca rápida e eficiente de Pokémon</li>
+                                        <li>✅ Interface limpa e organizada</li>
+                                        <li>✅ Navegação fácil entre as rotas</li>
+                                        <li>✅ Design responsivo</li>
+                                        <li>✅ Experiência do usuário excepcional</li>
+                                </ul>
+
+                                <p>
+                                        <strong>Objetivo:</strong> Esta Pokedex se utiliza de um backend feito na linguagem Golang, esse backend 
+                                        que é o centro da aplicação, você pode ver ele clicando aqui: <a target="_blank" href="https://github.com/Fsp30/Pokedex_with_golang">Backend Golang</a>
+                                </p>
+                        </section>
+                        </main>
+
+                </>
+        )
+}
